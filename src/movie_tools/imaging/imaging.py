9faabe6c36c8ee9b852584ef4f63e43b8b149974
shamada <shamada@jeol.co.jp>
2022-07-14 09:44:22 +0900

    FEAT: Create function to draw text anywhere

--- src/movie_tools/imaging/imaging.py
+++ src/movie_tools/imaging/imaging.py
@@ -3,19 +3,40 @@ from pathlib import Path
 from PIL import Image, ImageFont, ImageDraw
 
 
-def main():
-    image_path = r".\test.png"
-    text = "Hello World"
-    color = (255, 255, 255)  # 文字の色
-    font_path = Path(__file__).parent.joinpath("fonts", "Roboto-Bold.ttf")
-    font_size = 200  # 文字の大きさ
+class Imaging:
+    DEFAULT_FONT_SIZE = 64
 
-    font = ImageFont.truetype(font=str(font_path), size=font_size)
-    image = Image.new(mode="RGBA", size=(1920, 1080), color=(0, 0, 0, 0))
-    draw = ImageDraw.Draw(image)
-    draw.text((110, 20), text, fill=color, font=font)
-    image.save(image_path, quality=95)
+    def __init__(self):
+        self.img = Image.new(mode="RGBA", size=(1920, 1080), color=(0, 0, 0, 0))
+
+        self.font_path = Path(__file__).parent.joinpath("fonts", "Roboto-Bold.ttf")
+        self.font_size = 20  # 文字の大きさ
+
+    def build(self, image_path: str = r".\test.png"):
+        self.img.save(image_path, quality=95)
+
+    def write(
+        self,
+        text: str,
+        xy: tuple[float, float] = (110, 20),
+        size: int = DEFAULT_FONT_SIZE,
+    ):
+        """
+        白抜き黒枠の文字を画像上に描画
+        位置とテキスト内容、サイズを変更可能
+        """
+        font = ImageFont.truetype(font=str(self.font_path), size=size)
+        draw = ImageDraw.Draw(self.img)
+        draw.text(
+            xy,
+            text,
+            fill=(255, 255, 255),
+            font=font,
+            stroke_fill="black",
+            stroke_width=10,
+        )
 
 
-if __name__ == "__main__":
-    main()
+i = Imaging()
+i.write("Hello world")
+i.build()
